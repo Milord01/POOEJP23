5333e68af0c3f9083d722fd9f9a54ca9f4068e9c
Camilord01 <124930730+Milord01@users.noreply.github.com>
2023-09-18 14:43:39 -0500

    Carga inicial

---
+++ src/pooejp23/POOEJP23.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pooejp23;
+
+/**
+ *
+ * @author MILO GAVIRIA
+ */
+public class POOEJP23 {
+
+    public static void main(String[] args) {
+        Ecuacion2do form= new Ecuacion2do();
+        form.setVisible(true);
+    }
+    
+}
